1963fb26ba87f896c1f956a48fdaf45a15fc0cbf
Belina-LPS <82065894+Belina-LPS@users.noreply.github.com>
2026-05-17 02:25:18 -0700

    Add "body" screen and write instructions for coding a new section

--- main.js
+++ main.js
@@ -8,6 +8,66 @@
   // implement the rest of expletives in DECtalk,
   // implement what other sound files we did
   
+// M:
+  // To add a new category,
+    // On the left side of the screen, you'll see a preview of the program.
+    // Above it are three tabs: Code, Design, and Data.
+    // Switch to the Design tab.
+    // The program should default to the "mainMenu" screen.
+    // There will be a dropdown list from that label.
+    // Switch to any other screen (mainMenu doesn't have a home button).
+    // The right panel will have some info about the screen,
+    // along with a Duplicate button.
+    // Duplicate that screen.
+    // Now you get to rename all the UI yaaayyy
+    // Decide on a nickname for your screen.
+      // For example, I've used "mm" for main menu, or "ex" for expletives
+    // Click on a UI element (such as the home button) and rename it accordingly.
+      // e.g. The play button on the main menu is called "mm_play"
+    // Rename all word buttons (eg. "ex_boo" is "boo" on the expletives page).
+    // Add your nickname to the global scrns array in this code.
+    
+  // To add a word,
+    // Get the word as a .mp3 file.
+      // For DECtalk, I've been using a certain site that exports .wavs,
+      // then converting them to .mp3s
+    // Get the length of that file.
+      // I've been using an online audio trimming service that displays
+      // the length of the sound in milliseconds.
+    // Upload the word to this project.
+      // Switch this code to blocks by hitting the
+      // Show Blocks button in the top right corner.
+      // The function below (do_nothing_help_me_upload_sound_lol) will have a
+      // dropdown menu. Open it and hit Choose...
+      // It will let you upload sound.
+    // Add the word to the corresponding array.
+      // At this point, I only have DECtalk files.
+      // So I'll use the soundfiles_DECtalk array as an exmaple.
+      // Objects are weird in JavaScript. Just don't look them directly in the eye.
+      // Example object in the array:
+        // {word: "am", file: "am_DECtalk.mp3", dur: 766}
+      // This word is "am", so the word member is assigned "am".
+      // The file is assigned the word plus an underscore and the voice type;
+      // in this case, "am"+"_"+"DECtalk" -> "am_DECtalk" ... .mp3.
+      // The duration (dur) is the duration of the sound in milliseconds.
+      // Let's say you're adding "quail" with a hypothetical length of 700ms.
+        // {word: "quail", file: "quail_DECtalk.mp3", dur: 700}
+      // Be sure to insert the soundfile in sorted order, by *filename*.
+        // Not by word.
+        
+  // To make this all actually function,
+    // Hurrah! You're onto the actual coding stage.
+    // Separate off some space for your code.
+    // You'll see I use a buncha hashtags. Copy that for consistency.
+    // Copy the code for the UI at the top from another screen
+    // and replace the nickname with your new screen's nickname.
+      // These elements are home, voice, play, backspace, delete, speak,
+      // confirm_del, confirm_delY, and confirm_delN.
+      // You may also want a back button.
+    // Copy the code for any clickable word and replace it with
+    // the name of your UI element (word button) and replace the word
+    // in the function call with your new word.
+  
 function do_nothing_help_me_upload_sound_lol() {
   playSound("");
 }
@@ -21,7 +81,7 @@ function do_nothing_help_me_upload_sound_lol() {
 // #######################################################################
 
 // list of screen nicknames
-var scrns = ["mm", "ex", "v1", "v2"];
+var scrns = ["mm", "ex", "v1", "v2", "bd"];
 
 // Currently selected voice
 // Options: DECtalk
@@ -48,33 +108,49 @@ var durQueue = [];
 var soundfiles_DECtalk = [
   {word: "am", file: "am_DECtalk.mp3", dur: 766},
   {word: "are", file: "are_DECtalk.mp3", dur: 876},
+  {word: "arm", file: "arm_DECtalk.mp3", dur: 966},
+  {word: "back", file: "back_DECtalk.mp3", dur: 966},
   {word: "be", file: "be_DECtalk.mp3", dur: 857},
   {word: "boo", file: "boo_DECtalk.mp3", dur: 902},
+  {word: "breathe", file: "breathe_DECtalk.mp3", dur: 966},
+  {word: "chest", file: "chest_DECtalk.mp3", dur: 1159},
   {word: "close", file: "close_DECtalk.mp3", dur: 1045},
   {word: "come", file: "come_DECtalk.mp3", dur: 883},
   {word: "damn", file: "dmn_DECtalk.mp3", dur: 985},
   {word: "drink", file: "drink_DECtalk.mp3", dur: 1050},
   {word: "eat", file: "eat_DECtalk.mp3", dur: 805},
+  {word: "face", file: "face_DECtalk.mp3", dur: 1037},
   {word: "fuck", file: "fck_DECtalk.mp3", dur: 927},
   {word: "fucking", file: "fcking_DECtalk.mp3", dur : 1043},
+  {word: "foot", file: "foot_DECtalk.mp3", dur: 953},
   {word: "get", file: "get_DECtalk.mp3", dur: 831},
   {word: "give", file: "give_DECtalk.mp3", dur: 883},
   {word: "go", file: "go_DECtalk.mp3", dur: 844},
+  {word: "hair", file: "hair_DECtalk.mp3", dur: 966},
+  {word: "hand", file: "hand_DECtalk.mp3", dur: 1154},
   {word: "have", file: "have_DECtalk.mp3", dur: 882},
   {word: "help", file: "help_DECtalk.mp3", dur: 953},
   {word: "hi", file: "hi_DECtalk.mp3", dur: 940},
   {word: "hide", file: "hide_DECtalk.mp3", dur: 1063},
+  {word: "eye", file: "i_DECtalk.mp3", dur: 892},
+  {word: "i", file: "i_DECtalk.mp3", dur: 892},
   {word: "is", file: "is_DECtalk.mp3", dur: 934},
+  {word: "leg", file: "leg_DECtalk.mp3", dur: 992},
   {word: "make", file: "make_DECtalk.mp3", dur: 951},
+  {word: "mouth", file: "mouth_DECtalk.mp3", dur: 1030},
+  {word: "nail", file: "nail_DECtalk.mp3", dur: 927},
   {word: "need", file: "need_DECtalk.mp3", dur: 992},
+  {word: "nose", file: "nose_DECtalk.mp3", dur: 1076},
   {word: "open", file: "open_DECtalk.mp3", dur: 966},
   {word: "ow", file: "ow_DECtalk.mp3", dur: 902},
   {word: "pause", file: "pause_DECtalk.mp3", dur: 1146},
+  {word: "period", file: "period_DECtalk.mp3", dur: 1275},
   {word: "put", file: "put_DECtalk.mp3", dur: 953},
   {word: "show", file: "show_DECtalk.mp3", dur: 998},
   {word: "shit", file: "sht_DECtalk.mp3", dur: 934},
   {word: "spook", file: "spook_DECtalk.mp3", dur: 1082},
   {word: "stay", file: "stay_DECtalk.mp3", dur: 1030},
+  {word: "stomach", file: "stomach_DECtalk.mp3", dur: 1146},
   {word: "stop", file: "stop_DECtalk.mp3", dur: 1114},
   {word: "to", file: "to_DECtalk.mp3", dur: 934},
   {word: "touch", file: "touch_DECtalk.mp3", dur: 1030},
@@ -82,9 +158,11 @@ var soundfiles_DECtalk = [
   {word: "uhh", file: "uhh_DECtalk.mp3", dur: 788},
   {word: "understand", file: "understand_DECtalk.mp3", dur: 1288},
   {word: "use", file: "use_DECtalk.mp3", dur: 921},
+  {word: "wah", file: "wah_DECtalk.mp3", dur: 927},
   {word: "wait", file: "wait_DECtalk.mp3", dur: 966},
   {word: "want", file: "want_DECtalk.mp3", dur: 934},
   {word: "will", file: "will_DECtalk.mp3", dur: 908},
+  {word: "yeah", file: "yeah_DECtalk.mp3", dur: 966},
   {word: "yippee", file: "yippee_DECtalk.mp3", dur:1050}
   ];
 
@@ -340,9 +418,15 @@ onEvent("mm_expletives", "click", function( ) {
   setScreen("expletives");
 });
 
-
 onEvent("mm_to", "click", function() {
   append_word("to");
+});
+
+onEvent("mm_body", "click", function( ) {
+  // switch screen to expletives screen
+  console.log("mm -> bd");
+  form_sentence_and_update();
+  setScreen("body");
 });
 
 onEvent("mm_verbs1", "click", function( ) {
@@ -446,9 +530,127 @@ onEvent("ex_uhh", "click", function() {
   append_word("uhh");
 });
 
+onEvent("ex_wah", "click", function() {
+  append_word("wah");
+});
+
+onEvent("ex_yeah", "click", function() {
+  append_word("yeah");
+});
+
 onEvent("ex_yippee", "click", function() {
   append_word("yippee");
 });
+
+
+// #######################################################################
+//          ####################### BODY #######################
+// #######################################################################
+
+// ################ TOOLBAR ################
+
+onEvent("bd_home", "click", function( ) {
+  // switch screen to main menu
+  console.log("bd -> mm");
+  form_sentence_and_update();
+  setScreen("mainMenu");
+});
+
+onEvent("bd_voice", "click", function( ) {
+  // cycle voice
+  console.log("Bd: Cycle voice");
+});
+
+onEvent("bd_play", "click", function( ) {
+  // play queue
+  console.log("Bd: Play");
+  say_queue();
+});
+
+onEvent("bd_backspace", "click", function( ) {
+  // remove last item in queue
+  console.log("Bd: Backspace");
+  backspace();
+});
+
+onEvent("bd_delete", "click", function( ) {
+  // delete entire queue
+  console.log("V1: Confirming deletion of queue");
+  open_confirm_popup("bd");
+});
+
+onEvent("bd_confirm_delN", "click", function( ) {
+  // close the popup and do nothing
+  close_confirm_popup("bd");
+});
+
+onEvent("bd_confirm_delY", "click", function( ) {
+  // close the popup and delete the queues
+  console.log("V1: clearing queues");
+  delete_sentence();
+  close_confirm_popup("bd");
+});
+
+
+// ################ GRID ################
+
+onEvent("bd_arm", "click", function() {
+  append_word("arm");
+});
+
+onEvent("bd_back", "click", function() {
+  append_word("back");
+});
+
+onEvent("bd_chest", "click", function() {
+  append_word("chest");
+});
+
+onEvent("bd_eye", "click", function() {
+  append_word("eye", "i");
+});
+
+onEvent("bd_face", "click", function() {
+  append_word("face");
+});
+
+onEvent("bd_foot", "click", function() {
+  append_word("foot");
+});
+
+onEvent("bd_hair", "click", function() {
+  append_word("hair");
+});
+
+onEvent("bd_hand", "click", function() {
+  append_word("hand");
+});
+
+onEvent("bd_leg", "click", function() {
+  append_word("leg");
+});
+
+onEvent("bd_mouth", "click", function() {
+  append_word("mouth");
+});
+
+onEvent("bd_nail", "click", function() {
+  append_word("nail");
+});
+
+onEvent("bd_nose", "click", function() {
+  append_word("nose");
+});
+
+onEvent("bd_period", "click", function() {
+  append_word("period");
+});
+
+onEvent("bd_stomach", "click", function() {
+  append_word("stomach");
+});
+
+
 
 
 
@@ -503,6 +705,10 @@ onEvent("v2_confirm_delY", "click", function( ) {
 
 
 // ################ GRID ################
+
+onEvent("v2_breathe", "click", function() {
+  append_word("breathe");
+});
 
 onEvent("v2_close", "click", function() {
   append_word("close");
